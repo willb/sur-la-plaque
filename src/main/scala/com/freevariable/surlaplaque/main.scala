2a653d6a1e39cb295b0b9dbf0188677f15f4d211
William Benton <self@willbenton.com>
2013-10-17 17:56:26 -0500

    main app skeleton

---
+++ src/main/scala/com/freevariable/surlaplaque/main.scala
@@ -0,0 +1,26 @@
+package com.freevariable.surlaplaque;
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+
+import com.freevariable.surlaplaque.importer._
+
+class SLP(sc: SparkContext) {
+    def main(files: Array[String]) {
+        val datasets = files.map((file: String) => sc.parallelize(extract.trackpointDataFromFile(file)))
+    }
+}
+
+object Main {
+    
+    def main(args: Array[String]) {
+        val appName = "sur-la-plaque"
+        val master = sys.env.get("SLP_MASTER") match {
+            case Some(v) => v
+            case None => "local[2]"
+        }
+        
+        // XXX: add optional parameters here to support cluster execution
+        val app = new SLP(new SparkContext(master, appName))
+    }
+}
