a0a2636dc875e0ce7dd95a9f8ef09a31bd9c7783
William Benton <self@willbenton.com>
2013-10-24 13:49:49 -0500

    Data bucketing

--- src/main/scala/com/freevariable/surlaplaque/data.scala
+++ src/main/scala/com/freevariable/surlaplaque/data.scala
@@ -8,6 +8,26 @@ sealed case class Trackpoint(timestamp: Long, latlong: Coordinates, altitude: Do
     val timestring = Timestamp.stringify(timestamp)
 }
 
+object ZoneHistogram {
+    def make(ftp:Int) = {
+        case class Buckets(z1:Long, z2:Long, z3:Long, z4:Long, z5:Long, z6:Long, z7:Long) {
+            def record(sample:Double, ct:Long = 1) = sample match {
+                case d:Double if d <= ftp * 0.55 => new Buckets(z1+ct,z2,z3,z4,z5,z6,z7)
+                case d:Double if d <= ftp * 0.75 => new Buckets(z1,z2+ct,z3,z4,z5,z6,z7)
+                case d:Double if d <= ftp * 0.9 => new Buckets(z1,z2,z3+ct,z4,z5,z6,z7)
+                case d:Double if d <= ftp * 1.05 => new Buckets(z1,z2,z3,z4+ct,z5,z6,z7)
+                case d:Double if d <= ftp * 1.2 => new Buckets(z1,z2,z3,z4,z5+ct,z6,z7)
+                case d:Double if d <= ftp * 1.5 => new Buckets(z1,z2,z3,z4,z5,z6+ct,z7)
+                case d:Double if d > ftp * 1.5 => new Buckets(z1,z2,z3,z4,z5,z6,z7+ct)
+                case _ => this
+            }
+                        
+            def +(other: Buckets) = new Buckets(z1+other.z1, z2+other.z2, z3+other.z3, z4+other.z4, z5+other.z5, z6+other.z6, z7+other.z7)
+        }
+        new Buckets(0,0,0,0,0,0,0)
+    }
+}
+
 object Timestamp {
     def stringify(ts: Long) = ts.toDateTime.toString()
 }
--- src/main/scala/com/freevariable/surlaplaque/main.scala
+++ src/main/scala/com/freevariable/surlaplaque/main.scala
@@ -4,15 +4,16 @@ import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 
 import com.freevariable.surlaplaque.importer._
+import com.freevariable.surlaplaque.data._
 
 class SLP(sc: SparkContext) {
     def processFiles(files: Array[String], mmpPeriod: Int = 60) = {
-        val tupleSets = files.map((_, extract.trackpointDataFromFile(_)))
-        
+        val points = files.flatMap((s:String) => extract.trackpointDataFromFile(s))
+        sc.parallelize(points)
     }
 }
 
-object Main {
+object BucketApp {
     
     def main(args: Array[String]) {
         val appName = "sur-la-plaque"
@@ -21,9 +22,25 @@ object Main {
             case None => "local[2]"
         }
         
+        val ftp = sys.env.get("SLP_FTP") match {
+            case Some(v) => v.toInt
+            case None => 300
+        }
+        
         // XXX: add optional parameters here to support cluster execution
         val app = new SLP(new SparkContext(master, appName))
         
+        val data = app.processFiles(args)
         
+        val emptyBuckets = ZoneHistogram.make(ftp)
+        
+        val counts = data.map((tp:Trackpoint) => (tp.watts)).countByValue
+        
+        val buckets = counts.foldLeft(emptyBuckets)((b, tup:Pair[Double,Long]) => {
+            val (w,ct) = tup
+            b.record(w,ct)
+            })
+        
+        Console.println(buckets)
     }
 }
