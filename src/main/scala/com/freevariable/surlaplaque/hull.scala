52c5f31eb073ac72dab051f4cecc067c5118b95b
William Benton <self@willbenton.com>
2013-11-02 22:14:41 -0500

    geometric primitive WIP

--- src/main/scala/com/freevariable/surlaplaque/hull.scala
+++ src/main/scala/com/freevariable/surlaplaque/hull.scala
@@ -1,3 +1,4 @@
+
 package com.freevariable.surlaplaque.quickhull;
 
 import com.freevariable.surlaplaque.data._
@@ -18,5 +19,18 @@ object QuickHull {
     val Coordinates(by,bx) = b
     val Coordinates(cy,cx) = c
     (ax*by) - (ay*bx) + (ay*cx) - (ax*cy) + (bx*cy) - (cx*by)
+  }
+  
+  sealed abstract class Position {}
+  case class Above extends Position {}
+  case class Below extends Position {}
+  case class Upon extends Position {}
+  
+  def relativePosition(point:Coordinates, line_start:Coordinates, line_end:Coordinates) = {
+      triDet(line_start, line_end, point) match {
+          case 0 => Upon()
+          case x if x > 0 => Above()
+          case x if x < 0 => Below()
+      }
   }
 }
