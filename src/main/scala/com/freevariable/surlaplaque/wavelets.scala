e3a553ef20ce57ca6f4c98fdfad0f3d79b96eb7d
William Benton <self@willbenton.com>
2014-02-18 09:25:40 -0600

    idiomaticy police strike again

--- src/main/scala/com/freevariable/surlaplaque/wavelets.scala
+++ src/main/scala/com/freevariable/surlaplaque/wavelets.scala
@@ -11,12 +11,14 @@ object WaveletExtractor {
     private def waveletize(samples: Array[Double]) = haarTr(DenseVector(samples)).toArray
     
     private def tossQuietest(samples:  Array[Double], keepRatio: Double) = {
-        val keepCount = (keepRatio * samples.length).toInt
+        val keepCount = ((keepRatio * samples.length).toInt) + 1
         val sorted = collection.SortedSet(samples.toArray :_*)
         val minMag = (sorted.takeRight(keepCount) | sorted.take(keepCount)).map(math.abs(_)).takeRight(keepCount).min
         
         samples.map((smp) => if (math.abs(smp) < minMag) 0.0 else smp)
     }
 
-    def extractWavelets(samples: Array[Double], windowSize: Int = 1024, skip: Int = 30, keepRatio: Double = 0.15) = samples.sliding(windowSize, skip).toList.par.map((darr) => tossQuietest(waveletize(darr), keepRatio))
+    def transformAndAbstract(samples: Array[Double], windowSize: Int = 1024, skip: Int = 30, keepRatio: Double = 0.15) = 
+        for (window <- samples.sliding(windowSize, skip))
+            yield tossQuietest(waveletize(window), keepRatio)
 }
