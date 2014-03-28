237dd6075451244f2c9dafec6748da9558d83956
William Benton <self@willbenton.com>
2014-03-28 14:21:59 -0500

    added StreamRecorder to Util

--- src/main/scala/com/freevariable/surlaplaque/util.scala
+++ src/main/scala/com/freevariable/surlaplaque/util.scala
@@ -7,3 +7,28 @@ object PowerQuantizer {
     }
 }
 
+case class StreamRecorder(count: Long, min: Double, max: Double, mean: Double, sumX2: Double) {
+  import Math.sqrt
+
+  def add(sample: Double) = {
+    val dev = sample - mean
+    val newCount = count + 1
+    val newMean = mean + (dev / newCount)
+    
+    StreamRecorder(
+      newCount, 
+      if (sample < min) sample else min,
+      if (sample > max) sample else max,
+      newMean,
+      sumX2 + (dev * (sample - newMean))
+      )
+  }
+  
+  def variance = sumX2 / count
+  def stddev = sqrt(variance)
+}
+
+object StreamRecorder {
+  import java.lang.{Double => JDouble}
+  def empty = new StreamRecorder(0, JDouble.MAX_VALUE, JDouble.MIN_VALUE, 0, 0)
+}
