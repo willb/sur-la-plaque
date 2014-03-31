b5821881f3242a755ca62fd265e788eab4ac2af0
William Benton <self@willbenton.com>
2014-03-30 21:40:38 -0500

    Added comparisons for coordinate data

--- src/main/scala/com/freevariable/surlaplaque/data.scala
+++ src/main/scala/com/freevariable/surlaplaque/data.scala
@@ -2,10 +2,37 @@ package com.freevariable.surlaplaque.data;
 
 import com.github.nscala_time.time.Imports._
 
-sealed case class Coordinates(lat: Double, lon: Double) {
-    import com.freevariable.surlaplaque.util.RWDistance.{distance => rw_distance}
-    
-    def distance(other:Coordinates) = rw_distance((lat, lon), (other.lat, other.lon))
+sealed case class Coordinates(lat: Double, lon: Double) extends Ordered[Coordinates] {
+  import com.freevariable.surlaplaque.util.RWDistance.{distance => rw_distance}
+  
+  /**
+    Approximate distance between this and other in meters
+  */
+  def distance(other:Coordinates) = rw_distance((lat, lon), (other.lat, other.lon))
+
+  /**
+    Ordering based on longitude then latitude
+  */
+  def compare(other: Coordinates) = 
+    implicitly[Ordering[(Double, Double)]].compare((this.lon, this.lat), (other.lon, other.lat))
+  
+  /** 
+    Ordering based on latitude (then longitude, if necessary) 
+  */
+  def compare_lat(other: Coordinates) = 
+    implicitly[Ordering[(Double, Double)]].compare((this.lat, this.lon), (other.lat, other.lon))
+  
+  /**
+    Returns true if this is strictly "to the left of" (viz., to the west of, or to the south of if this and other are equally-westerly) other
+    */
+  def strictlyLeft(other: Coordinates) = (this compare other) < 0
+  
+  def strictlyRight(other: Coordinates) = (this compare other) > 0
+  
+  def strictlyAbove(other: Coordinates) = (this compare_lat other) > 0
+  
+  def strictlyBelow(other: Coordinates) = (this compare_lat other) < 0
+
 }
 
 sealed case class Trackpoint(timestamp: Long, latlong: Coordinates, altitude: Double, watts: Double, activity: Option[String]) {
