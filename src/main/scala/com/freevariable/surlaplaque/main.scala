0d0bfa5d445acb63ee27b3a4d2585ea46621c5c0
William Benton <self@willbenton.com>
2014-03-31 15:00:34 -0500

    Made app objects available from main namespace

--- src/main/scala/com/freevariable/surlaplaque/main.scala
+++
@@ -1,8 +0,0 @@
-package com.freevariable.surlaplaque;
-
-import com.freevariable.surlaplaque.app.BucketApp
-import com.freevariable.surlaplaque.app.BucketClusterApp
-import com.freevariable.surlaplaque.app.ReplHarness
-import com.freevariable.surlaplaque.app.GPSClusterApp
-import com.freevariable.surlaplaque.app.MMPClusterApp
-import com.freevariable.surlaplaque.app.WaveletClusterApp
---
+++ src/main/scala/com/freevariable/surlaplaque/package.scala
@@ -0,0 +1,12 @@
+package com.freevariable
+
+import com.freevariable.surlaplaque.app
+
+package object surlaplaque {
+  val BucketApp = app.BucketApp
+  val BucketClusterApp = app.BucketClusterApp
+  val ReplHarness = app.ReplHarness
+  val GPSClusterApp = app.GPSClusterApp
+  val MMPClusterApp = app.MMPClusterApp
+  val WaveletClusterApp = app.WaveletClusterApp
+}
