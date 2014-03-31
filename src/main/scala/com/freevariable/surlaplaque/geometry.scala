33949f0eacd6a616a939cd6db43a334705a50d53
William Benton <self@willbenton.com>
2014-03-30 21:42:06 -0500

    initial/slow convex hull implementation

---
+++ src/main/scala/com/freevariable/surlaplaque/geometry.scala
@@ -0,0 +1,46 @@
+// not-particularly-robust geometrical primitives
+
+package com.freevariable.surlaplaque
+
+import com.freevariable.surlaplaque.data.{Coordinates, Trackpoint}
+
+trait GeometryPrimitives {
+  // TODO:  add some basic primitives here to enable pushing more of
+  // convex hull creation out to RDD-land
+}
+
+object ConvexHull {
+  def calculate(points: List[Coordinates]): List[Coordinates] = {
+    val sortedPoints = points.sorted.toSet.toList
+    
+    if (sortedPoints.length <= 1) {
+      sortedPoints
+    } else {
+      val _::lowerHull = buildHull(points, List())
+      val _::upperHull = buildHull(points.reverse, List())
+      lowerHull.reverse ++ upperHull.reverse
+    }
+  }
+  
+  private[this] def turnType(a:Coordinates, b:Coordinates, c:Coordinates) = {
+    val Coordinates(ax,ay) = a
+    val Coordinates(bx,by) = b
+    val Coordinates(cx,cy) = c
+    (ax*by) - (ay*bx) + (ay*cx) - (ax*cy) + (bx*cy) - (cx*by)
+  }
+  
+  private[this] def buildHull(points: List[Coordinates], hull: List[Coordinates]): List[Coordinates] = {
+    (points, hull) match {
+      case (Nil, _) => hull
+      case (p::ps, first::second::rest) => {
+        if(turnType(second, first, p) <= 0) {
+          buildHull(points, second::rest)
+        } else {
+          buildHull(ps, p::first::second::rest)
+        }
+      }
+      case (p::ps, first::Nil) => buildHull(ps, p::first::Nil)
+      case (p::ps, Nil) => buildHull(ps, p::Nil)
+    }
+  }
+}
