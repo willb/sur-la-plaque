d0a195ce392e06ea4ea352ec783d547b91b6e2a3
William Benton <self@willbenton.com>
2014-03-31 19:29:31 -0500

    GPS app:  added hulls.
    The GPS clustering app now generates convex hulls for each cluster.

--- src/main/scala/com/freevariable/surlaplaque/app/gps.scala
+++ src/main/scala/com/freevariable/surlaplaque/app/gps.scala
@@ -13,6 +13,8 @@ import com.freevariable.surlaplaque.app._
 object GPSClusterApp extends Common {
     import spray.json._
     import DefaultJsonProtocol._
+    import org.apache.spark.rdd.RDD
+    import com.freevariable.surlaplaque.util.ConvexHull
     
     def main(args: Array[String]) {
         // XXX: add optional parameters here to support cluster execution
@@ -23,31 +25,48 @@ object GPSClusterApp extends Common {
         val numClusters = getEnvValue("SLP_CLUSTERS", "128").toInt
         val numIterations = getEnvValue("SLP_ITERATIONS", "20").toInt
         
-        val vectors = data.map((tp:Trackpoint) => Array(tp.latlong.lat, tp.latlong.lon)).cache()
-        val km = new KMeans()
-        km.setK(numClusters)
-        km.setMaxIterations(numIterations)
+        val model = clusterPoints(data, numClusters, numIterations)
         
-        val model = km.run(vectors)
+        val labeledVectors = data.keyBy((tp:Trackpoint) => model.predict(Array(tp.latlong.lat, tp.latlong.lon)))
         
-        val labeledVectors = vectors.map((arr:Array[Double]) => (model.predict(arr), arr))
+        val hulls = generateHulls(data, model)
         
-        val counts = labeledVectors.countByKey
+        val hullPolys = hulls.map({case (cluster, coords) => makeHullPoly(cluster, coords)}).collect
         
-        val maxCount = counts.map({case (_,v) => v}).max
-        
-        val points = counts.map({case (cluster,count) => makePointMap(cluster, count, model.clusterCenters(cluster), maxCount)})
-        
-        val struct = Map("type"->"FeatureCollection".toJson, "features"->points.toJson)
+        val struct = Map("type"->"FeatureCollection".toJson, "features"->hullPolys.toJson)
         
         val out = outputFile
         out.println(struct.toJson)
         out.close
     }
 
+    def clusterPoints(rdd: RDD[Trackpoint], numClusters: Int, numIterations: Int) = {
+      val km = new KMeans()
+      km.setK(numClusters)
+      km.setMaxIterations(numIterations)
+      
+      val vecs = rdd.map(tp => Array(tp.latlong.lon, tp.latlong.lat)).cache()
+      km.run(vecs)
+    }
+
+    def generateHulls(rdd: RDD[Trackpoint], model: KMeansModel) = {
+      val clusteredPoints = rdd.groupBy((tp:Trackpoint) => model.predict(Array(tp.latlong.lon, tp.latlong.lat)))
+      clusteredPoints.map({case (ctr, pts) => (ctr, ConvexHull.calculate(pts.map(_.latlong).toList))})
+    }
+
+    def makeHullPoly(cluster:Int, coords:List[Coordinates]) = {
+      val acoords = coords.map(c => Array(c.lon, c.lat).toJson).toArray
+
+      Map(
+        "type" -> "Feature".toJson,
+        "geometry" -> Map("type"->"Polygon".toJson, "coordinates"->Array((acoords ++ Array(acoords(0))).toJson).toJson).toJson,
+        "properties" -> Map("fill"->rgb((cluster*2).toByte, 0, 0)).toJson
+      )
+    }
+
     def makePointMap(cluster:Int, count:Long, coords:Array[Double], max:Long) = {
         val frac = count.toDouble / max
-        val color = rgb(frac)
+        val color = var_gb(frac)
         val ssize = symsize(frac)
         Map(
         "type" -> "Feature".toJson,
@@ -62,8 +81,10 @@ object GPSClusterApp extends Common {
         case _ => "large"
     }
     
-    def rgb(frac:Double) = {
+    def var_gb(frac:Double) = {
         val gb = ((1-frac) * 256).toInt
         "#ff%02x%02x".format(gb,gb)
     }
+    
+    def rgb(r: Byte, g: Byte, b: Byte) = "#%02x%02x%02x".format(r,g,b)
 }
